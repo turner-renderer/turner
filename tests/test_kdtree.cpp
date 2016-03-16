52b6f471896acb8812110600579f03cbeb520d29
dima <d@zerovolt.org>
2016-03-16 22:54:26 +0100

    Remove internal KDTree Plane datatype.
    Former-commit-id: 846ded88acfd455aac794b60cb5b58d69c123332

--- tests/test_kdtree.cpp
+++ tests/test_kdtree.cpp
@@ -11,15 +11,15 @@ TEST_CASE("Split box at plane", "[box]") {
     Box box{{0, 0, 0}, {2, 2, 2}};
 
     Box l, r;
-    std::tie(l, r) = split(box, Plane{Axis::X, 1});
+    std::tie(l, r) = split(box, Axis::X, 1);
     REQUIRE(l == (Box{{0, 0, 0}, {1, 2, 2}}));
     REQUIRE(r == (Box{{1, 0, 0}, {2, 2, 2}}));
 
-    std::tie(l, r) = split(box, Plane{Axis::Y, 1});
+    std::tie(l, r) = split(box, Axis::Y, 1);
     REQUIRE(l == (Box{{0, 0, 0}, {2, 1, 2}}));
     REQUIRE(r == (Box{{0, 1, 0}, {2, 2, 2}}));
 
-    std::tie(l, r) = split(box, Plane{Axis::Z, 1});
+    std::tie(l, r) = split(box, Axis::Z, 1);
     REQUIRE(l == (Box{{0, 0, 0}, {2, 2, 1}}));
     REQUIRE(r == (Box{{0, 0, 1}, {2, 2, 2}}));
 }
