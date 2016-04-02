209e9ab21869c3df6dae65f60ce33f7dc267bd5f
Karsten Jeschkies <jeskar@web.de>
2016-04-02 12:20:35 +0200

    Define union member in Vec2.

--- lib/types.h
+++ lib/types.h
@@ -59,14 +59,16 @@ Vec operator/(int a, const Vec& v);
 
 struct Vec2 {
 
-//    Vec2(): x(0.0f), y(0.0f) {}
-//    constexpr Vec2(float _x, float _y) : x(_x), y(_y) {}
-
     float operator[](unsigned int i) const {
         return *(&x + i);
     }
 
-    float x, y;
+    union {
+        struct {
+            float x, y;
+        };
+        float vec[2];
+    };
 };
 
 using Color = aiColor4D;
