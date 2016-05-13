ddef527de6b4f5f4c3cb7b1f88d0ba32a12440f6
Karsten Jeschkies <jeskar@web.de>
2016-05-13 09:13:30 +0200

    Move sampling implementation to cpp.

--- lib/sampling.h
+++ lib/sampling.h
@@ -7,45 +7,19 @@
 #include <cmath>
 #include <tuple>
 
-namespace {
-
-static constexpr float M_2PI = 2.f * M_PI;
-
-}
-
 namespace sampling {
 
-__thread static xorshift64star<float> uniform{4};
-
-std::pair<Vec, float> hemisphere() {
-    // draw coordinates
-    float u1 = uniform();
-    float u2 = uniform();
-
-    // u1 is cos(theta)
-    auto z = u1;
-    auto r = sqrtf(fmax(0.f, 1.f - z*z));
-    auto phi = M_2PI * u2;
-    auto x = r * cosf(phi);
-    auto y = r * sinf(phi);
-    return std::make_pair(Vec{x, y, z}, u1);
-}
+/**
+ * Sample a point on a hemisphere.
+ */
+std::pair<Vec, float> hemisphere();
 
 /**
  * Sample a point on the triangle.
  *
  * return point in world space.
  */
-Vec triangle(const Triangle& triangle) {
-    while(true) {
-        float r1 = uniform();
-        float r2 = uniform();
-
-        if ((r1 + r2) <= 1.f) {
-            return triangle.vertices[0] + r1 * triangle.u + r2 * triangle.v;
-        }
-    }
-}
+Vec triangle(const Triangle& triangle);
 
 }
 
