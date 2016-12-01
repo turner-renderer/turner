e37082d41aa292b9045a736d1be279a33dce9b5e
boxdot <d@zerovolt.org>
2016-12-01 01:27:58 +0100

    WIP: Fix normal calc in triangle.
    Former-commit-id: a815de16888a296cef3248d05ad711626bf80933

--- lib/radiosity.cpp
+++ lib/radiosity.cpp
@@ -19,101 +19,12 @@
 #include "sampling.h"
 
 /**
- * The form factor between faces `from` (i) and `to` (j) is defined as
- *
- * F_ij = 1/A_i ∫_x ∫_y G'(x, y) dA_y dA_x
- *      ≈ 1/A_j A_j/L ∑_{l = 1...L} (A_i/K ∑_{k = 1...K} G'(x^l, y^k))
- *      = A_j/N ∑_{n = 1...N} G'(x^n, y^n)
- *
- * Here, x and y are points on face i and j, A_i is the area of i, and A_i, and
- * A_j are infinitesimal areas around x and y. Further,
- *
- * G'(x, y) = V(x, y) * cos+(θ_i) * cos+(θ_j) / (π * ||x-y||^2), where
- *
- * V(x, y) - visibility indicator between x and y (1 if visible, 0 else)
- * θ_i - angle between normal of x and vector to y (ω)
- * θ_j - angle between normal of y and vector to x (-ω)
- *
- * The above integrals are approximated by Monte-Carlo.
- */
-float form_factor(const KDTree& tree, const KDTree::TriangleId from_id,
-                  const KDTree::TriangleId to_id, const size_t num_samples) {
-    assert(from_id != to_id);
-
-    const auto& from = tree[from_id];
-    const auto& to = tree[to_id];
-
-    float result = 0;
-    for (size_t i = 0; i != num_samples; ++i) {
-        auto p1 = sampling::triangle(from);
-        auto p2 = sampling::triangle(to);
-
-        auto v = p2 - p1;
-        if (tree.intersect(Ray{p1 + EPS * v, v}) != to_id) {
-            continue;
-        }
-
-        auto square_length = v.SquareLength();
-
-        v.Normalize();
-        float cos_theta1 = v * from.normal;
-        float cos_theta2 = -v * to.normal;
-
-        float G = cos_theta1 * cos_theta2 / square_length;
-        if (G > 0) {
-            result += G;
-        }
-    }
-
-    return M_1_PI * result * to.area() / num_samples;
-}
-
-/**
- * As above function.
- *
- * Instead of using triangles from the given kd-tree, computes the form factors
- * for the directly given triangles. The triangle `to` has to be a subtriangle
- * of the triangle in the kd-tree defined by `to_id`.
- */
-float form_factor(const KDTree& tree, const Triangle& from, const Triangle& to,
-                  const KDTree::TriangleId to_id, const size_t num_samples) {
-    float result = 0;
-    for (size_t i = 0; i != num_samples; ++i) {
-        auto p1 = sampling::triangle(from);
-        auto p2 = sampling::triangle(to);
-
-        auto v = p2 - p1;
-        if (tree.intersect(Ray{p1 + EPS * v, v}) != to_id) {
-            continue;
-        }
-
-        auto square_length = v.SquareLength();
-
-        v.Normalize();
-        float cos_theta1 = v * from.normal;
-        float cos_theta2 = -v * to.normal;
-
-        float G = cos_theta1 * cos_theta2 / square_length;
-        if (G > 0) {
-            result += G;
-        }
-    }
-
-    return M_1_PI * result * to.area() / num_samples;
-}
-
-/**
  * [CW93], Algorithm 4.21 without obvious mistakes.
  */
-float form_factor_expiremental(const KDTree& tree,
-                               const KDTree::TriangleId from_id,
+float form_factor_expiremental(const KDTree& tree, const Triangle& from,
+                               const Triangle& to,
                                const KDTree::TriangleId to_id,
                                const size_t num_samples) {
-    assert(from_id != to_id);
-
-    const auto& from = tree[from_id];
-    const auto& to = tree[to_id];
-
     float result = 0;
     for (size_t i = 0; i != num_samples; ++i) {
         auto p1 = sampling::triangle(from);
@@ -137,4 +48,74 @@ float form_factor_expiremental(const KDTree& tree,
     }
 
     return result * from.area() / num_samples;
+}
+
+float form_factor_expiremental(const KDTree& tree,
+                               const KDTree::TriangleId from_id,
+                               const KDTree::TriangleId to_id,
+                               const size_t num_samples) {
+    assert(from_id != to_id);
+
+    const auto& from = tree[from_id];
+    const auto& to = tree[to_id];
+
+    return form_factor_expiremental(tree, from, to, to_id, num_samples);
+}
+
+/**
+ * The form factor between faces `from` (i) and `to` (j) is defined as
+ *
+ * F_ij = 1/A_i ∫_x ∫_y G'(x, y) dA_y dA_x
+ *      ≈ 1/A_i A_j/L ∑_{l = 1...L} (A_i/K ∑_{k = 1...K} G'(x^l, y^k))
+ *      = A_j/N ∑_{n = 1...N} G'(x^n, y^n)
+ *
+ * Here, x and y are points on face i and j, A_i is the area of i, and A_i, and
+ * A_j are infinitesimal areas around x and y. Further,
+ *
+ * G'(x, y) = V(x, y) * cos+(θ_i) * cos+(θ_j) / (π * ||x-y||^2), where
+ *
+ * V(x, y) - visibility indicator between x and y (1 if visible, 0 else)
+ * θ_i - angle between normal of x and vector to y (ω)
+ * θ_j - angle between normal of y and vector to x (-ω)
+ *
+ * The above integrals are approximated by Monte-Carlo.
+ */
+float form_factor(const KDTree& tree, const Triangle& from, const Triangle& to,
+                  const KDTree::TriangleId to_id, const size_t num_samples) {
+    float result = 0;
+    for (size_t i = 0; i != num_samples; ++i) {
+        auto p1 = sampling::triangle(from);
+        auto p2 = sampling::triangle(to);
+
+        auto v = p2 - p1;
+        if (tree.intersect(Ray{p1 + EPS * from.normal, v}) != to_id) {
+            continue;
+        }
+
+        float square_length = v.SquareLength();
+
+        float cos_theta1 = v * from.normal;
+        if (cos_theta1 < 0) {
+            continue;
+        }
+
+        float cos_theta2 = (-v) * to.normal;
+        if (cos_theta2 < 0) {
+            continue;
+        }
+
+        float G = cos_theta1 * cos_theta2 / square_length;
+        result += G;
+    }
+
+    return M_1_PI * result * to.area() / num_samples;
+}
+
+float form_factor(const KDTree& tree, const KDTree::TriangleId from_id,
+                  const KDTree::TriangleId to_id, const size_t num_samples) {
+    assert(from_id != to_id);
+    const auto& from = tree[from_id];
+    const auto& to = tree[to_id];
+
+    return form_factor(tree, from, to, to_id, num_samples);
 }
