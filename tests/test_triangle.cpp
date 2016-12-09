583e95b66768437aa7edf0afd38757a4589f49b8
Karsten Jeschkies <jeskar@web.de>
2016-12-09 22:06:12 +0100

    Test normal interpolation for triangle.
    Former-commit-id: a46fef6e00bf1abf590189b3de39bab42928540b

--- tests/test_triangle.cpp
+++ tests/test_triangle.cpp
@@ -12,7 +12,7 @@ TEST_CASE("Test Triangle normal", "[triangle]")
 
         Vec normal = triangle.normal;
 
-        // Verify unit lenght of normal.
+        // Verify unit length of normal.
         float length = normal.Length();
         REQUIRE(length == Approx(1.f));
 
@@ -21,5 +21,30 @@ TEST_CASE("Test Triangle normal", "[triangle]")
         float cos_v = normal * triangle.v.Normalize();
         REQUIRE(cos_u == Approx(0.f));
         REQUIRE(cos_v == Approx(0.f));
+    }
+}
+
+TEST_CASE("Test interpolate triangle normal", "[triangle]")
+{
+    static constexpr int NUM_SAMPLES = 100;
+
+    for (int j = 0; j < NUM_SAMPLES; ++j) {
+        Triangle triangle = test_triangle( random_vec(), random_vec(),
+            random_vec(), random_vec().Normalize(), random_vec().Normalize(),
+            random_vec().Normalize());
+
+        float r = uniform();
+        float s = uniform();
+        float t = uniform();
+        while( r+s+t > 1.0f) {
+          r = uniform();
+          s = uniform();
+          t = uniform();
+        }
+        Vec normal = triangle.interpolate_normal(r, s, t);
+
+        // Verify unit length of normal.
+        float length = normal.Length();
+        REQUIRE(length == Approx(1.f));
     }
 }
