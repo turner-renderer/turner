5728057574d08a9c7548d62ebddc9df5f8232d8d
Karsten Jeschkies <jeskar@web.de>
2017-02-11 13:38:43 +0100

    Install imagemagick and render cornell box.

--- scripts/render-samples.sh
+++ scripts/render-samples.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 set -ex
 
+if [ $(uname) = "Linux" ]; then
+  apk add --no-cache imagemagick
+fi
+
 scene=$1
 output=$2
 fullname=$(basename $scene)
 filename=${fullname%.*}
 
-./raycaster $scene | convert - $output/${filename}_raycast.png
-./raytracer $scene | convert - $output/${filename}_raytrace.png
-./pathtracer -p1 -m1 $scene | convert - $output/${filename}_pathtrace.png
-./radiosity direct -e10 $scene | convert - $output/${filename}_raddir.png
-./radiosity hierarchical --gouraud --max-subdivisions=1 -e10 $scene \
-	| convert - $output/${filename}_radhier.png
+mkdir -p "$output"
+build/raycaster $scene | convert - $output/${filename}_raycast.png
+build/raytracer $scene | convert - $output/${filename}_raytrace.png
+build/pathtracer -p1 -m1 $scene | convert - $output/${filename}_pathtrace.png
+build/radiosity exact -e10 $scene | convert - $output/${filename}_rad_exact.png
+build/radiosity hierarchical --gouraud --max-subdivisions=1 -e10 $scene \
+	| convert - $output/${filename}_rad_hierarchical.png
